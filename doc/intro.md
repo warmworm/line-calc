25653fe676a72098aa32758134556704bc2dd34e
warmworm <saladinham@gmail.com>
2016-11-07 11:23:41 +0900

    * 프로젝트 뼈대 추가

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to line-calc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/line_calc/core_test.clj
@@ -0,0 +1,7 @@
+(ns line-calc.core-test
+  (:require [clojure.test :refer :all]
+            [line-calc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
