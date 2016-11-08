c8a96cacf0ccba4d939ab3cc8723b904a28ba5d8
warmworm <saladinham@gmail.com>
2016-11-08 13:30:43 +0900

    * 프로젝트 셋팅 수정 및 작업 일지 추가

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject line-calc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Expression Calculator"
+  :url "https://github.com/warmworm/line-calc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
