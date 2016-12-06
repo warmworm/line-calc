42d4feced2094bc5d5de43b8b7911898c25b667b
saladinham <saladinham@gmail.com>
2016-12-07 03:43:33 +0900

    * 임시 UI 띄우기

--- src/line_calc/core.clj
+++ src/line_calc/core.clj
@@ -6,7 +6,18 @@
 ;; Author: warmworm
 ;;
 
-(ns line-calc.core)
+(ns line-calc.core
+  (:import (javax.swing JFrame JPanel JLabel JButton)))
+
 
 (defn -main [& args]
-  (println "Hello, line-calc"))
+  (let [frame (javax.swing.JFrame. "line-calc")
+        panel (javax.swing.JPanel.)]
+    (doto panel
+      (.setSize (java.awt.Dimension. 800 250))
+      (.setVisible true))
+    (doto frame
+      (.add panel)
+      (.pack)
+      (.setVisible true))))
+
