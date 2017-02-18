a4221715992be0c8ac4a1fcbec0a050309be38b2
saladinham <saladinham@gmail.com>
2017-02-18 09:55:19 +0900

    * UI 코드 수정

--- src/line_calc/ui.clj
+++ src/line_calc/ui.clj
@@ -17,6 +17,7 @@
 ; create a calculator UI
 (defn create-ui [title]
   (let [frame (JFrame. title)
+        log-panel (JPanel.)
         panel (JPanel.)
         log (JTextArea. 5 20)
         label (JLabel. "Expression:")
@@ -25,25 +26,32 @@
 
     ;(doto label
     ;  (.setPreferredSize (Dimension. 150 50)))
+
+    (doto log
+      (.setSize (Dimension. 300 300)))
     
     (doto exp
-      (.setPreferredSize (Dimension. 500 50))
+      (.setSize (Dimension. 500 50))
       (.setColumns 50)
       (.setText "Input Expression"))
 
     (doto button
-      (.setPreferredSize (Dimension. 150 50))
+      (.setSize (Dimension. 150 50))
       (.addActionListener
        (proxy [ActionListener] []
          (actionPerformed [_]
            (.append log (str (.getText exp) \newline))))))
+
+    (doto log-panel
+      (.setLayout (BorderLayout.))
+      (.add log BorderLayout/CENTER))
     
     (doto panel
-      (.setLayout (FlowLayout.))
-      (.add log)
-      (.add label)
-      (.add exp)
-      (.add button))
+      (.setLayout (BorderLayout.))
+      (.add log-panel BorderLayout/NORTH)
+      (.add label BorderLayout/WEST)
+      (.add exp BorderLayout/CENTER)
+      (.add button BorderLayout/EAST))
     
     (doto frame
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
